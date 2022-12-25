be0e2af7c1da37cf2fb752e82a9a28e34cf6122b
marcus1337 <blurgas@hotmail.com>
2022-12-25 12:26:33 +0100

    remove test input

--- test/test.cpp
+++ test/test.cpp
@@ -8,12 +8,6 @@ int main() {
 
     NeuralNet nn(4, 2);
     std::cout << "Test prediction: " << nn.predict({0.5,0.4,0.3,0.2}) << "\n";
-    
-    std::string exitStr = "n";
-    while (exitStr != "y") {
-        std::cout << "Type 'y' to exit.\n";
-        std::cin >> exitStr;
-    }
 
     return 0;
 }
